67b30421240aebbd86bf7d0c85df1e91b3c90478
Morten Rask Jensen <morten_rask_jensen@hotmail.com>
2020-10-26 23:48:22 +0100

    Add Tail

---
+++ cs/Tail/Tail/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                Run r = new Run(args[0], args[1]);
+            }
+            else
+                Console.WriteLine("filename bytes");
+        }
+    }
+}
---
+++ cs/Tail/Tail/Tail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tail
+{
+    class Run
+    {
+        public Run(string filename, string leng)
+        {
+            int n;
+            if (int.TryParse(leng, out n))
+                Tail(filename, n);
+        }
+
+        private void Tail(string fn, int n)
+        {
+            using (var reader = new StreamReader(fn))
+            {
+                if (reader.BaseStream.Length > n)
+                {
+                    reader.BaseStream.Seek(-n, SeekOrigin.End);
+                }
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
